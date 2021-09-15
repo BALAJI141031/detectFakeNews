56d9b3cd8e8eefb1e1b16aca4832f4dca6bd2c75
BALAJI141031 <85923874+BALAJI141031@users.noreply.github.com>
2021-09-15 21:19:25 +0530

    final-Commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # detectFakeNews
- 
+
+simple CLI app which detects the fake news if the source for that news belongs to facebook telegram instagram
---
+++ index.js
@@ -0,0 +1,8 @@
+// const readline = require("readline-sync");
+// readline();
+
+// // var userName = prompt("give data:");
+
+// // console.log(userName);
+
+const readline = require("readline-sync");
